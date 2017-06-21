7d85140bb5559ed8453ca0aba353a815b198afe2
SCONSULTING AF10 <SCONSULTING AF10@sconsultingaf10.decorcenter.corp>
2017-06-21 17:05:59 -0500

    buscar Solicitante, servicios: añadirMaterial, guardarDocumento, y btnRefresh

--- services/clienteServices.js
+++ services/clienteServices.js
@@ -16,6 +16,36 @@ sap.ui.define([
 
             return utilService.exec(contexto);
 
+        },
+
+
+        buscarSolicitante: function(codigo,ruc,dni,codcliente,descripcion,
+                                    direccion,distrito,telefono,mail,esRuc,canal) {
+
+            var contexto = {};
+            contexto.servicio = "clienteServices.buscarSolicitante()";
+            contexto.url = "buscarClientes.aspx";
+            contexto.parametros = { Codigo : codigo , Ruc: ruc , dni: dni , codcliente: codcliente, Descripcion: descripcion,
+                                    Direccion: direccion, CodigoPostal:distrito, Telefono: telefono, Mail: mail,esRuc: esRuc,canal:canal};
+
+            return utilService.exec(contexto);
+
+        },
+
+
+        buscarSolicitanteNombre:function(BusNombres,NombresBuscado){
+            var contexto = {};
+            contexto.servicio = "clienteServices.buscarSolicitanteNombre()";
+            contexto.url = "buscarClientes.aspx";
+            contexto.parametros = {BusNombres: BusNombres,NombresBuscado: NombresBuscado};
+
+            return utilService.exec(contexto);
         }
+
+
+
+
+
+
     };
 });
