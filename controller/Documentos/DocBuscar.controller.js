05d7ab60d8898c65318d1c084a086d81aa7d5f71
AF 542 <AF 542@AF-542.decorcenter.corp>
2017-07-17 10:16:32 -0500

    Hecho Quejas e imprimir

--- controller/Documentos/DocBuscar.controller.js
+++ controller/Documentos/DocBuscar.controller.js
@@ -16,6 +16,7 @@ sap.ui.define([
             window.numeroDocumento = null;
         },
         onRouteMatched: function (oEvent) {
+            
             var oData = {
                 datosBuscarDoc: {
                     "tipoBusqueda": "1",
