d1f3cf7c8f65829a914c9e4f9b40b1de0908c272
SCONSULTING AF678 <SCONSULTING AF678@192.168.14.111>
2017-07-19 17:54:14 -0500

    Cambios Stock por pedir 19/07/2017

--- controller/Stock/StockporPedir.controller.js
+++ controller/Stock/StockporPedir.controller.js
@@ -14,54 +14,70 @@ sap.ui.define([
 			oRouter.attachRoutePatternMatched(this.onRouteMatched, this);
 			
 		},
-		onRouteMatched: function(oEvent) {
-            if (oEvent.getParameter("name") == "appStockporPedir") {
+		onRouteMatched: function(oEvent) 
+		{
+			var date = new Date();
+            var yyyy = date.getFullYear().toString();
+            var mm = (date.getMonth() + 1).toString(); // getMonth() is zero-based
+            var dd  = date.getDate().toString();
+            var fechaActual = yyyy +"-"+ (mm[1] ? mm : "0" + mm[0]) +"-"+ (dd[1] ? dd : "0" + dd[0]);
+            if (oEvent.getParameter("name") == "appStockporPedir") 
+            {
             	this.getView().setModel(new JSONModel({}));
-                    this.getView().getModel().setProperty("/dataIni",window.dataIni);
-                    this.getView().getModel().setProperty("/codMaterial",window.codMaterial);
-                    this.getView().getModel().refresh(true);
-					this.getView().byId("dlg_stockPorPedir").open();
-					
-                };
+                this.getView().getModel().setProperty("/dataIni",window.dataIni);
+                this.getView().getModel().setProperty("/codMaterial",window.codMaterial);
+                this.getView().getModel().refresh(true);
+				this.getView().byId("dlg_stockPorPedir").open();
+				this.getView().byId("date_fechaInicio_stockPorPedir").setValue(fechaActual);
+           		this.getView().byId("date_fechaFin_stockPorPedir").setValue(fechaActual);	
+            };
 		},
 		
 		
-		onAfterRendering:function(){
+		onAfterRendering:function()
+		{
 		},
 		//Boton Home
-		goHome:function(){
+		goHome:function()
+		{
             var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                 oRouter.navTo("appHome");
         },
 		
 		//Cerrar Dialog Doc Nuevo Inicio
-		onContinuarStockPorPedir: function(oEvent) {
-
-				var CodJer = this.getView().byId("com_codJerarqui_stockPorPedir").getSelectedKey();
-				if(CodJer==" "){
-					CodJer="";
-				}
-                var CodMat = this.getView().byId("txt_codMaterial_stockPorPedir").getValue();
-                var	FecIni = this.getView().byId("date_fechaInicio_stockPorPedir").getValue();
-                var FecFin = this.getView().byId("date_fechaFin_stockPorPedir").getValue();
-                var OfVentas = window.dataIni.person.OfVentas;
-                var result = stockServices.stockporPedir(CodJer,CodMat,FecIni,FecFin,OfVentas);  
-                if (result.c === "s") {
-                                if (result.data.success) {
-                                   this.getView().getModel().setProperty("/retornoStockPorPedir", result.data);
-                                   this.getView().byId("dlg_stockPorPedir").close();
-                                   
-                                } else {
-                                    sap.m.MessageToast.show(result.data.errors.reason, {
-                                        duration: 3000
-                                    });
-                                }
-                            } else {
-                                sap.m.MessageToast.show(result.m, {
-                                    duration: 3000
-                                });
-                            }
-                            console.log(result.data);
+		onContinuarStockPorPedir: function(oEvent) 
+		{
+			var CodJer = this.getView().byId("com_codJerarqui_stockPorPedir").getSelectedKey();
+			if(CodJer==" ")
+			{
+				CodJer="";
+			}
+            var CodMat = this.getView().byId("txt_codMaterial_stockPorPedir").getValue();
+            var	FecIni = this.getView().byId("date_fechaInicio_stockPorPedir").getValue();
+            var FecFin = this.getView().byId("date_fechaFin_stockPorPedir").getValue();
+            var OfVentas = window.dataIni.person.OfVentas;
+            var result = stockServices.stockporPedir(CodJer,CodMat,FecIni,FecFin,OfVentas);  
+            if (result.c === "s") 
+            {
+                if (result.data.success) 
+                {
+                   this.getView().getModel().setProperty("/retornoStockPorPedir", result.data);
+                   this.getView().byId("dlg_stockPorPedir").close(); 
+                } 
+                else 
+                {
+                    sap.m.MessageToast.show(result.data.errors.reason, {
+                        duration: 3000
+                    });
+                }
+            } 
+            else 
+            {
+                sap.m.MessageToast.show(result.m, {
+                    duration: 3000
+                });
+            }
+            console.log(result.data);
 		},
             
 	});
